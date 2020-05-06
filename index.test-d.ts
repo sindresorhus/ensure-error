8d4107424aaeb107d45f5eb36e6dbee85b800134
Pedro Augusto de Paula Barbosa <papb1996@gmail.com>
2020-05-06 13:13:30 -0300

    Improve TypeScript return type when `any` is inferred (#2)

--- index.test-d.ts
+++ index.test-d.ts
@@ -3,5 +3,6 @@ import ensureError = require('.');
 
 const error = new TypeError('🦄');
 
+expectType<Error>(ensureError(10 as any));
 expectType<TypeError>(ensureError(error));
 expectType<ensureError.NonError>(ensureError(10));
