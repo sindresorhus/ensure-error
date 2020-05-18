d691bb882f13cae155538dd4aad21aec80995d85
papb <papb1996@gmail.com>
2020-05-17 22:30:18 -0300

    Guarantee `stack` property in TypeScript typings

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,8 +1,18 @@
-import {expectType} from 'tsd';
+import {expectAssignable, expectType} from 'tsd';
 import ensureError = require('.');
+
+type WithStack<T> = T & { stack: string; };
 
 const error = new TypeError('🦄');
 
-expectType<Error>(ensureError(10 as any));
-expectType<TypeError>(ensureError(error));
+expectAssignable<Error>(ensureError(10 as any));
+expectAssignable<TypeError>(ensureError(error));
+
+expectType<WithStack<Error>>(ensureError(10 as any));
+expectType<WithStack<TypeError>>(ensureError(error));
 expectType<ensureError.NonError>(ensureError(10));
+
+// Ensure stack property
+expectType<string>(ensureError(10).stack);
+expectType<string>(ensureError(10 as any).stack);
+expectType<string>(ensureError(error).stack);
